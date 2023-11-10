10a41abed381479ce48c64c321aa89bc62b35707
Asilaaa <147584814+Asilaaa@users.noreply.github.com>
2023-11-10 10:37:08 +0300

    add online IDE url

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,28 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-
+    std::string result;
+    switch (S) {
+        case 'M':
+            if (height < 1.70) {
+                result = "Short";
+            } else if (height >= 1.70 && height <=1.85) {
+                result = "Normal";
+            } else {
+                result = "Tall";
+            }
+            break;
+        case 'F':
+            if (height < 1.60) {
+                result = "Short";
+            } else if (height >= 1.60 && height <=1.75) {
+                result = "Normal";
+            } else {
+                result = "Tall";
+            }
+            break;
+    }
+    return result;
     // use return to return your result
     // make use of control flow statements
 }
