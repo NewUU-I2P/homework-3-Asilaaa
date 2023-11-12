1542601a43ce4988133ff465af65cbd07e8f9724
Asilaaa <147584814+Asilaaa@users.noreply.github.com>
2023-11-12 09:48:21 +0300

    add online IDE url

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,19 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-
+    switch (int(operation)) {
+        case 42:
+            result = x * y;
+            break;
+        case 43:
+            result = x + y;
+            break;
+        case 45:
+            result = x - y;
+            break;
+        case 47:
+            result = x / y;
+            break;
+    }
    return result;
 }
