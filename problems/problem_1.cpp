9648367d1ca72a83cf37d410d59ae0779252b7d1
Asilaaa <147584814+Asilaaa@users.noreply.github.com>
2023-11-13 10:36:45 +0300

    add online IDE url

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,19 +1,37 @@
 float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
+    float RemainingAmountOfWater;
     cost = 13;
 
-    if (consumed_water <= 30) {
+    if (consumed_water >= 30) {
         cost += 30 * 0.4;
-    }
-    else if (consumed_water <=50){
-        cost += 30 * 0.4 + (consumed_water - 30) * 0.12;
-    }
-    else if (consumed_water <=60){
-        cost += 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
-    }
-    else {
-        cost += 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
-    }
+        RemainingAmountOfWater = consumed_water - 30;
+        if (RemainingAmountOfWater >= 20) {
+            cost += 20 * 0.12;
+            RemainingAmountOfWater -= 20;
+            if (RemainingAmountOfWater >= 10) {
+                cost += 10 * 1.4;
+                RemainingAmountOfWater -= 10;
+                cost += RemainingAmountOfWater * 1.5;
+            } else {
+                cost += RemainingAmountOfWater * 1.5;
+            }
+        } else if (RemainingAmountOfWater >= 10) {
+            cost += 10 * 1.4;
+            RemainingAmountOfWater -= 10;
+            cost += RemainingAmountOfWater * 1.5;
+        }
+    } else if (RemainingAmountOfWater >= 20) {
+        cost += 20 * 0.12;
+        RemainingAmountOfWater -= 20;
+        if (RemainingAmountOfWater >= 10) {
+            cost += 10 * 1.4;
+            RemainingAmountOfWater -= 10;
+            cost += RemainingAmountOfWater * 1.5;
+        } else {
+            cost += RemainingAmountOfWater * 1.5;
+        }
         return cost;
     }
+}
